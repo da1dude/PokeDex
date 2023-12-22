42143e5797098294da20003db06e9259f5572a97
Paul K <pkerschieter@gmail.com>
2023-12-22 09:16:43 -0600

    configured authentication

---
+++ server.js
@@ -0,0 +1,60 @@
+////////////////////////////////
+// Import Dependencies      ////
+////////////////////////////////
+const express = require('express') // import express framework
+require('dotenv').config() // import/load ENV variables
+const path = require('path') // import path module
+const middleware = require('./utils/middleware')
+///////////////////////
+// Import Routers  ////
+///////////////////////
+const UserRouter = require('./controllers/userControllers')
+//const PlaceRouter = require('./controllers/placeControllers')
+
+//////////////////////////////////////////////////
+// Create the app object + set up view engine ////
+//////////////////////////////////////////////////
+const app = express() // call the express function
+
+// view engine - ejs
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+///////////////////
+// Middleware  ////
+///////////////////
+middleware(app)
+
+///////////////
+// Routes  ////
+///////////////
+// basic home route
+app.get('/', (req, res) => {
+    const { username, loggedIn, userId } = req.session
+    // res.send('the app is connected')
+    res.render('home.ejs', { username, loggedIn, userId })
+})
+
+app.use('/users', UserRouter)
+//app.use('/places', PlaceRouter)
+
+// error page
+app.get('/error', (req, res) => {
+    const error = req.query.error || 'Something went wrong...try again'
+
+    const { username, loggedIn, userId } = req.session
+
+    // res.send(error)
+    res.render('error.ejs', { error, userId, username, loggedIn })
+})
+
+////////////////////////
+// Server Listener  ////
+////////////////////////
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log('Your server is running')
+})
+
+// End
